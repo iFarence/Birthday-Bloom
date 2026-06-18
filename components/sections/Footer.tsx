3fc787d6994f1dced5839b396dfc4eb07f08efc0
iFarence <divaanggunil025@gmail.com>
2026-06-18 14:58:01 +0700

    fix footer

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -2,7 +2,7 @@ import { eventData } from '@/constants/data'
 
 export default function Footer() {
   return (
-    <footer className="bg-midnight py-16 px-6">
+    <footer className="bg-midnight py-5 px-6">
       <div className="max-w-xl mx-auto flex flex-col items-center gap-6 text-center">
         <h2 className="font-script text-5xl text-vanilla">Terima kasih</h2>
         <div className="w-12 h-px bg-blush/50" />
